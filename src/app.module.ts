b9b56fcdf2f0994ef1dc534afa89eb5ca4f64f5c
Craig Roberts <git@craig.craig0990.co.uk>
2025-06-01 21:15:00 +0800

    chore: Add TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '1423'),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+      options: {
+        trustServerCertificate: true,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
